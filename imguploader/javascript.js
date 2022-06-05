b4f489622c9ae951b0aa754ddc3459cde80f5eb1
bogdanbv <bogdanweb3@gmail.com>
2022-06-05 21:17:38 +0200

    Add makeGrey function

--- imguploader/javascript.js
+++ imguploader/javascript.js
@@ -1,6 +1,20 @@
+var image;
+
 function upload() {
     var canvas = document.getElementById("can");
     var fileinput = document.getElementById("finput");
-    var image = new SimpleImage(fileinput);
+    image = new SimpleImage(fileinput);
+    // image.drawTo(canvas);
+}
+
+function makeGrey() {
+
+    for (var pixel of image.values()) {
+        var avg = (pixel.getRed() + pixel.getBlue() + pixel.getGreen())/3;
+        pixel.setRed(avg);
+        pixel.setGreen(avg);
+        pixel.setBlue(avg);
+    }
+    var canvas = document.getElementById("can");
     image.drawTo(canvas);
 }
